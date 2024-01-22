aae52b4f1ee012dc035daa848a38e5b33be12ef4
Angelo Gulina <agulina@gitlab.com>
2024-01-22 17:12:07 +0100

    💇‍ Update Post Model

--- src/models/post.ts
+++ src/models/post.ts
@@ -1,17 +1,17 @@
-import { PostResponse, Post } from "@/typings/post";
-import { PostDataService } from "@/services/abstract";
-import { MarkdownService } from "@/services/markdown";
+import { PostResponse, Post } from '@/typings/post'
+import { PostDataService } from '@/services/abstract'
+import { MarkdownService } from '@/services/markdown'
 
-const dataService = new MarkdownService();
+const dataService = new MarkdownService()
 
 export class PostModel {
-    constructor(protected readonly service: PostDataService = dataService) {}
+  constructor(protected readonly service: PostDataService = dataService) {}
 
-    public getAllPosts(): Promise<PostResponse[]> {
-        return this.service.getAvailablePosts();
-    }
+  public getAllPosts(): Promise<PostResponse[]> {
+    return this.service.getAvailablePosts()
+  }
 
-    public getPostBySlug(slug: Post['slug']): Promise<PostResponse> {
-        return this.getAllPosts().then(posts => posts.find(post => post?.slug === slug))
-    }
+  public getPostBySlug(slug: Post['slug']): Promise<PostResponse> {
+    return this.getPostBySlug(slug)
+  }
 }
