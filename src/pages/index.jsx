0c83e029f825eaa03ba4ab83478cb6e5406f075c
Angelo Gulina <agulina@gitlab.com>
2024-01-26 17:55:53 +0100

    👝 Small fixes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,7 +14,7 @@ export default function Index({ posts }) {
   return (
     <>
       <Head>
-        <title>fua - frequently used acronyms</title>
+        <title>fua | frequently used acronyms</title>
         <meta name="description" content="fua | frequently used acronyms" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
