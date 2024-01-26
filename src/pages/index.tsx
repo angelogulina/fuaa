ea9320171831f6beafca6eef805b7a456fab5d80
Angelo Gulina <agulina@gitlab.com>
2024-01-26 11:05:01 +0100

    🏡 Add index.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,35 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import Head from 'next/head'
+import { Open_Sans } from 'next/font/google'
+import { Post, PostRepository } from '@/repositories/post'
 
-const inter = Inter({ subsets: ["latin"] });
+const openSans = Open_Sans({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
 
-export default function Home() {
+type Props = {
+  posts: Post[]
+}
+
+export default function Index({ posts }: Props) {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      
-    </main>
-  );
+    <>
+      <Head>
+        <title>FUA - Frequently used acronyms</title>
+        <meta name="description" content="FUA - Frequently used acronyms" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={openSans.className}>
+        {posts.map((post) => post.title)}
+      </main>
+    </>
+  )
+}
+
+export const getStaticProps = async () => {
+  const repository = new PostRepository()
+  const posts = await repository.getAllPosts()
+  return { props: { posts } }
 }
