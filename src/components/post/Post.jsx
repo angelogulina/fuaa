b68fd9733f0b69bc749d167e559f3e43a4bc19d6
paolospoto <paolo.spoto@gmail.com>
2024-01-27 12:40:27 +0100

    fix: href

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -2,8 +2,10 @@ import styles from "@/components/post/index.module.scss";
 import Link from "next/link";
 
 const Post = ({ className, content, title }) => {
+  const lowerCaseTitle = title.toLowerCase();
+
   return (
-    <Link href={`/posts/${title}`} className={className}>
+    <Link href={`/posts/${lowerCaseTitle}`} className={className}>
       <h2 className={styles.Title}>{title}</h2>
       {content && <div>{content}</div>}
     </Link>
